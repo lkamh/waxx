f5b82f05874051056371994a13d49e004b32992c
lkamh <123456@gmail.com>
2023-12-13 17:02:39 +0800

    4

--- 9.js
+++ 9.js
@@ -102,7 +102,7 @@ device.keepScreenOn(3600 * 1000);
 fInfo("检测题库更新");
 fInfo("如果不动就是正在下载，多等会");
 const update_info = get_tiku_by_http("https://mirror.ghproxy.com/https://raw.githubusercontent.com/lkamh/waxx/main/info.json");
-fInfo("正在加载对战题库......请稍等\n题库版本：" + update_info["tiku_link"].split('_')[1]);
+fInfo("正在加载对战题库......请稍等\n题库版本:" + update_info["tiku_link"].split('_')[1]);
 var tiku = [];
 try {
   fInfo("题目数量:" + update_info["tiku_link"].split('_')[2].slice(0, -4));
