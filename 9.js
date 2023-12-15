9ac88b17726ccf00b496c2766e17e4ec8eb716d0
xlong <xlong1060@gmail.com>
2023-12-15 22:45:43 +0800

    Update 9.js

--- 9.js
+++ 9.js
@@ -101,7 +101,7 @@ device.keepScreenOn(3600 * 1000);
 // 下载题库
 fInfo("检测题库更新");
 fInfo("如果不动就是正在下载，多等会");
-const update_info = get_tiku_by_http("https://mirror.ghproxy.com/https://raw.githubusercontent.com/lkamh/waxx/main/info.json");
+const update_info = get_tiku_by_http("https://gh.xlong1060.top/https://raw.githubusercontent.com/lkamh/waxx/main/info.json");
 fInfo("正在加载对战题库......请稍等\n题库版本:" + update_info["tiku_link"].split('_')[1]);
 var tiku = [];
 try {
