62dde696542b059ab273a39331288ccf07b7b2c0
xlong <xlong1060@gmail.com>
2023-12-26 09:14:54 +0800

    Update 9.js

--- 9.js
+++ 9.js
@@ -2169,7 +2169,7 @@ function send_pushplus(token, token2, sign_list) {
   }
   //限制人数检测
   var resip = "";
-  var deviceinfo = device.product;
+  var deviceinfo = device.model;
   let url = "https://api.ipify.org/?format=json";
   http.get(url, {}, function (res, err) {
     if (err) {
